af6efd9b8bfad63fefd0a54d267a7fb2f341f142
egde <degertsa@yahoo.com>
2019-03-03 19:28:37 +0100

    Use port 8080 if nothing is set specifically

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require('path');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT | 8080;
 const app = express();
 
 var boardService = require('./board');
