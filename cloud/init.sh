49f41d897f8a51a4cdac3e234eec5edf954a921c
egde <degertsa@yahoo.com>
2019-03-03 16:42:50 +0100

    Added nginx config. Fixed some stuff in the init.sh script.

--- cloud/init.sh
+++ cloud/init.sh
@@ -9,3 +9,27 @@ nvm install stable
 nvm use stable
 
 sudo apt install nginx -y
+
+
+export PORT=8080
+sudo useradd -M app
+sudo usermod -aG app ubuntu
+
+sudo mkdir -p /app/retro
+sudo chown -R :app /app/retro
+sudo chmod -R g+rwX /app/retro
+sudo git clone https://github.com/egde/retro /app/retro
+
+cd /app/retro/frontend
+npm install
+npm run build
+
+cd /app/retro/backend
+npm install
+
+npm install -g pm2
+pm2 start /app/retro/backend/src/app.js
+
+mv -f /app/retro/cloud/default /etc/nginx/sites-available/default
+
+sudo systemctl restart nginx
